ee5cbbd06e3dae9430061d063e96ce6be17053ea
shivanii1401 <125336094+shivanii1401@users.noreply.github.com>
2023-08-11 11:54:34 +0530

    reactwork

---
+++ React-work/src/App.js
@@ -0,0 +1,18 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Home from './Home';
+import "bootstrap";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import $ from 'jquery';
+import Popper from 'popper.js';
+
+function App() {
+  return (
+    <>
+    <Home />
+    </>
+  );
+}
+
+export default App;
